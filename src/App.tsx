111c4704b1b7c1ad207801f95b55073130b64111
Tophe Ripley <bezedache29@gmail.com>
2022-12-29 23:21:13 +0100

    useRef, useState, useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
-import Card from './components/Card';
+import React, { useState, useRef, useEffect } from "react";
+import Card from "./components/Card";
 
 function App() {
+  const [cardsData, setCardsData] = useState([
+    {
+      title: "Picasso",
+      content: "Peintre XXème siècle",
+      id: 1,
+    },
+    {
+      title: "Van Gogh",
+      content: "Peintre XIXème siècle",
+      id: 2,
+    },
+  ]);
+
+  const btnRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    console.log(btnRef);
+
+    const handleResize = (e: Event) => {
+      console.log("RESIZE", e);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="App">
       <h1>Mon test</h1>
-      <Card title="Mon titre" content="Mon content" />
+      {cardsData.map((item) => (
+        <Card key={item.id} title={item.title} content={item.content} />
+      ))}
+      <button ref={btnRef}>Submit</button>
     </div>
   );
 }
